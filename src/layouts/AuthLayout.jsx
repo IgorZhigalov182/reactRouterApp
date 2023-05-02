040fc21d95190d6a9442873ce4b48c83592d00d3
Igor <zhigalovzenit112233@gmail.com>
2023-05-02 22:51:56 +0300

    create navigate component

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,31 +1,24 @@
 import React from 'react';
 // Librares
 import { useSelector } from 'react-redux';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 // Components
 import Card from '../components/Card';
-// Pages
-import LoginPage from '../pages/LoginPage';
-import SignUpPage from '../pages/SigupPage';
-// Store
+
 import { isLoggedInSelector } from '../store/authSlice';
 
 const AuthLayout = () => {
   //   let { path } = useRouteMatch();
   const isLoggedIn = useSelector(isLoggedInSelector());
 
-  //   if (isLoggedIn) {
-  // return <Redirect to="/" />;
-  //   }
+  if (isLoggedIn) {
+    return <Navigate to="/" />;
+  }
 
   return (
     <div className="flex grow flex-col justify-center items-center  dark:text-slate-200 ">
       <Card>
-        <Routes>
-          <Route path={'login'} element={<LoginPage />} />
-          <Route path={'signup'} element={<SignUpPage />} />
-          {/* <Redirect to={path + '/signup'} /> */}
-        </Routes>
+        <Outlet />
       </Card>
     </div>
   );
